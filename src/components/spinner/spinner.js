bc80fd9191ab1a619657a5aace02571a6a943e01
Rustam Botirov <rustambotirov88@gmail.com>
2019-11-30 21:38:48 +0200

    spinner added

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './spinner';
+
+const Spinner = () => {
+    const styleLoader = {
+        width: '100 %',
+        height: '100 %'
+    }
+    return (
+        <div className="loader-wrapper">
+            <div style={styleLoader} className="lds-double-ring">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
