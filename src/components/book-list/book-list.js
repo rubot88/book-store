6eb15e436755f0267bbd6f2ab76348d3fe48a880
Rustam Botirov <rustambotirov88@gmail.com>
2019-12-01 17:16:12 +0200

    dispatch data to store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import BookListItem from '../book-list-item';
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../actions';
+import { compose } from '../../utils';
 import './book-list.scss';
 
 class BookList extends Component {
+    componentDidMount() {
+        const { bookstoreService } = this.props;
+        const data = bookstoreService.getBooks();
+        this.props.booksLoaded(data);
+
+    }
     render() {
         const { books } = this.props;
         const bookList = books.map((book) => {
@@ -29,4 +38,9 @@ const mapStateToProps = ({ books }) => {
     };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = { booksLoaded };
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
