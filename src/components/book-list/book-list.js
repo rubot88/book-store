d1e6e4a0928e41413687fdb142ca4273f863ff46
Rustam Botirov <rustambotirov88@gmail.com>
2019-12-02 22:39:30 +0200

    use ownProps

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -11,19 +11,7 @@ import './book-list.scss';
 
 class BookList extends Component {
     componentDidMount() {
-        const {
-            bookstoreService,
-            booksLoaded,
-            booksRequested,
-            booksError } = this.props;
-        booksRequested();
-        bookstoreService.getBooks()
-            .then((data) => {
-                booksLoaded(data);
-            })
-            .catch((err) => {
-                booksError(err);
-            })
+        this.props.fetchBooks();
     }
     render() {
         const { books, loading, error } = this.props;
@@ -56,10 +44,18 @@ const mapStateToProps = ({ books, loading, error }) => {
     };
 };
 
-const mapDispatchToProps = {
-    booksLoaded,
-    booksRequested,
-    booksError
+const mapDispatchToProps = (dispatch,ownProps) => {
+    const {bookstoreService} = ownProps;
+    return {
+        fetchBooks: () => {
+
+            dispatch(booksRequested());
+            bookstoreService.getBooks()
+                .then((data) => dispatch(booksLoaded(data)))
+                .catch((err) => dispatch(booksError(err)));
+        }
+
+    }
 };
 
 export default compose(
